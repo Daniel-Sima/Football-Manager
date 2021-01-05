fdac88267f4d793fe59d99813f8a3e4ea7b59ca1
walty13 <walterbeles@gmail.com>
2021-01-05 15:13:34 +0100

    Update Team.java

--- Bin/Team/Team.java
+++ Bin/Team/Team.java
@@ -3,10 +3,13 @@ import java.util.ArrayList;
 public class Team{ //implements TeamLogo, TacticBoard{
 	private String name;
 	private ArrayList<Player> players = new ArrayList<Player>();
-	private final int HOME_BONUS = 5; // static final ?? 
+	private final int HOME_BONUS = 5; // static final ??
+	private int index;
+	private static int cpt = 0;
 
 	public Team(String name, ArrayList<Player> tabPlayer){
 		this.name = name;
+		this.index = cpt++;
 		for(int i=0; i < 16; i++)    //16 Players in a team
 			players.add(tabPlayer.get(i));
 	}
